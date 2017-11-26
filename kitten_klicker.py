b512b2b85e03b380bb3792499b9abefbf1497296
Rey Abolofia <rabolofia@newrelic.com>
2017-11-26 14:28:59 -0800

    Convert to browser based game.

--- kitten_klicker.py
+++ kitten_klicker.py
@@ -1,11 +1,10 @@
-import collections
-import os
+import aiohttp.web
+import aiohttp_jinja2
+import asyncio
+import jinja2
+import json
 import threading
 import time
-
-
-def _clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 class Building(object):
@@ -20,39 +19,33 @@ class Building(object):
 
 
 class ClickHandler(object):
-    def __init__(self, stats):
+    def __init__(self, ws, stats):
+        self.ws = ws
         self.stats = stats
 
-    def start(self):
-        _clear()
-        while True:
-            click = input(
-                'Press "s" to visit the store, otherwise attract a kitten\n')
-            if click == 's':
-                self.visit_store()
-            else:
-                with self.stats.kitten_count_lock:
-                    self.stats.kitten_count += 1
-            _clear()
-            print('There are now {} kittens'.format(self.stats.kitten_count))
+    async def start(self):
+        async for msg in self.ws:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                if msg.data == 'close':
+                    await self.ws.close()
+                else:
+                    await self.handle_click(msg.data)
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print('ws connection closed with exception %s' %
+                      self.ws.exception())
 
-    def visit_store(self):
-        _clear()
-        print('   Building                 |Price          |Production')
-        print('   -------------------------|---------------|----------')
-        for index, building in self.stats.store.items():
-            if self.stats.kitten_count < building.price:
-                break
-            price = str(building.price) + ' kittens'
-            print('{}) {}|{}|{} k/s'.format(
-                index, building.name.ljust(25), price.ljust(15),
-                building.prod))
-        print('\nYou currently have {} kittens '
-                'and are producing {} kittens per second'.format(
-                self.stats.kitten_count, self.stats.prod_per_sec))
+    async def handle_click(self, data):
+        if data.startswith('store_'):
+            self.visit_store(data)
+        else:
+            with self.stats.kitten_count_lock:
+                self.stats.kitten_count += 1
+        await self.ws.send_str(self.stats.to_json())
 
+    def visit_store(self, data):
         try:
-            selection = self.stats.store[int(input())]
+            selection = data.split('_')[1]
+            selection = self.stats.store[int(selection)]
         except (ValueError, KeyError):
             return
 
@@ -62,65 +55,93 @@ class ClickHandler(object):
         with self.stats.kitten_count_lock:
             self.stats.kitten_count -= selection.price
         self.stats.prod_per_sec += selection.prod
-        selection.price = int(selection.price * 1.15)
 
 
 class GameEngine(threading.Thread):
-    def __init__(self, stats, *args, **kwargs):
+    def __init__(self, ws, stats, *args, **kwargs):
         super(GameEngine, self).__init__(*args, **kwargs)
         self.daemon = True
 
         self._update_cycle = 1.0
+        self.ws = ws
         self.stats = stats
+        self.loop = asyncio.get_event_loop()
 
-    def update(self):
+    async def update(self):
         with self.stats.kitten_count_lock:
             self.stats.kitten_count += self.stats.prod_per_sec
+
+        await self.ws.send_str(self.stats.to_json())
 
     def run(self):
         update_delta = self._update_cycle
 
-        while True:
+        while not self.ws.closed:
             time.sleep(update_delta)
 
             start_time = time.time()
-            self.update()
+            asyncio.run_coroutine_threadsafe(self.update(), self.loop)
 
             elapsed_time = time.time() - start_time
             update_delta = self._update_cycle - elapsed_time
 
 
 class Stats(object):
+
+    store = [
+        Building(name='Empty Box', price=10, prod=1),
+        Building(name='Scratching Post', price=100, prod=10),
+        Building(name='Kitten Perch', price=1000, prod=100),
+        Building(name='Delux Scratching Post', price=10000, prod=1000),
+        Building(name='Delux Kitten Perch', price=100000, prod=10000),
+        Building(name='Kitten Hotel', price=1000000, prod=100000),
+    ]
+
     def __init__(self):
         self.kitten_count = 0
         self.kitten_count_lock = threading.Lock()
 
         self.prod_per_sec = 0
 
-        buildings = [
-            Building(name='Empty Box', price=10, prod=1),
-            Building(name='Scratching Post', price=100, prod=10),
-            Building(name='Kitten Perch', price=1000, prod=100),
-            Building(name='Delux Scratching Post', price=10000, prod=1000),
-            Building(name='Delux Kitten Perch', price=100000, prod=10000),
-            Building(name='Kitten Hotel', price=1000000, prod=100000),
-        ]
-        self.store = collections.OrderedDict((i, s) for i, s in
-                enumerate(buildings))
+    def to_json(self):
+        return json.dumps({
+            'kitten_count': self.kitten_count,
+            'prod_per_sec': self.prod_per_sec,
+        })
 
 
-def start_game():
+async def ws_communicate(request):
+    ws = aiohttp.web.WebSocketResponse()
+    await ws.prepare(request)
+
     # Create the stats object
     stats = Stats()
 
     # Start the game engine which holds the ioloop
-    game_engine = GameEngine(stats)
+    game_engine = GameEngine(ws, stats)
     game_engine.start()
 
-    # Start the click handler thread which will listen for click events
-    click_handler = ClickHandler(stats)
-    click_handler.start()
+    # Start the click handler which listens for click events
+    click_handler = ClickHandler(ws, stats)
+    await click_handler.start()
+
+    return ws
+
+
+@aiohttp_jinja2.template('index.html')
+async def game(request):
+    return {
+        'store': Stats.store,
+    }
 
 
 if __name__ == '__main__':
-    start_game()
+    app = aiohttp.web.Application()
+    app.router.add_static('/static', 'static')
+    aiohttp_jinja2.setup(app,
+            loader=jinja2.FileSystemLoader('templates'))
+
+    app.router.add_get('/ws', ws_communicate)
+    app.router.add_get('/', game)
+
+    aiohttp.web.run_app(app)
---
+++ static/main.js
@@ -0,0 +1,24 @@
+var ws = new WebSocket("ws://localhost:8080/ws");
+
+ws.onmessage = function (e) {
+    var value = JSON.parse(e.data);
+
+    var count = document.getElementById('count');
+    count.innerText = value.kitten_count;
+
+    var prod = document.getElementById('prod');
+    prod.innerText = value.prod_per_sec;
+}
+
+ws.onopen = function (e) {
+    var storeItems = document.getElementsByClassName('storeItem');
+    for(var i = 0; i < storeItems.length; i++) {
+        storeItems.item(i).onclick = function (e) {
+            ws.send(this.id);
+        }
+    };
+
+    document.onclick = function (e) {
+        ws.send('click');
+    }
+}
