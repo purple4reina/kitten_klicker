8304a9a1ffd417fc1081f2c6caed8016f9b03685
Rey Abolofia <rabolofia@newrelic.com>
2017-11-28 21:32:01 -0800

    Update kitten count 10 times/sec in UI.

--- static/main.js
+++ static/main.js
@@ -2,14 +2,24 @@ var host = window.location.host;
 var proto = (window.location.protocol == "https:") ? "wss://" : "ws://"
 var ws = new WebSocket(proto + host + "/ws");
 
+var count = 0;
+var prod = 0;
+var countElement;
+var prodElement;
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    countElement = document.getElementById('count');
+    prodElement = document.getElementById('prod');
+});
+
 ws.onmessage = function (e) {
     var value = JSON.parse(e.data);
 
-    var count = document.getElementById('count');
-    count.innerText = value.kitten_count;
+    count = value.kitten_count;
+    countElement.innerText = value.kitten_count;
 
-    var prod = document.getElementById('prod');
-    prod.innerText = value.prod_per_sec;
+    prod = value.prod_per_sec;
+    prodElement.innerText = value.prod_per_sec;
 }
 
 ws.onopen = function (e) {
@@ -29,4 +39,26 @@ ws.onopen = function (e) {
     kittenImage.onmouseup = function(e) {
         this.style.width = origSize;
     }
+
+    // Simulate continuous updating of the kitten count. When messages are
+    // receved from the server, those will take precedence.
+
+    var cycleTime = 0.1;
+
+    function sleep(sec) {
+        return new Promise(resolve => setTimeout(resolve, sec * 1000));
+    }
+
+    function update() {
+        count = count + Math.floor(prod * cycleTime);
+        countElement.innerText = count;
+    }
+
+    async function updateKittenCount() {
+        while (true) {
+            await sleep(cycleTime);
+            update();
+        }
+    };
+    updateKittenCount();
 }
